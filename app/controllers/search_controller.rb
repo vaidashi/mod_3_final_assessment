aa54603a1e9c8229328dd22d699567e002c0937d
Ashish Vaid <vaid50@yahoo.com>
2017-11-01 09:46:48 -0600

    added JSON parsing call in search index, will build full code here to get feature working first, need to figure out the exact faraday call next

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,13 @@
 class SearchController < ApplicationController
 
   def index
+    response = Faraday.get("")
 
-  end 
+    raw_stores = JSON.parse(response.body, symbolize_names: true)
+
+    @stores = raw_stores.map do |raw_store|
+      Store.new(raw_store)
+    end
+  end
 
 end
