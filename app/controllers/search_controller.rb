967e010acdaf506b3b853b678defff047c4b63fd
Ashish Vaid <vaid50@yahoo.com>
2017-11-01 10:28:24 -0600

    modified faraday call again to retrieve desired attributes and full count minus paginate

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,9 @@
 class SearchController < ApplicationController
 
   def index
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=20&apiKey=#{ENV["BEST_BUY_KEY"]}")
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name,city,distance,phone&pageSize=20&apiKey=#{ENV["BEST_BUY_KEY"]}")
 
-    raw_stores = JSON.parse(response.body, symbolize_names: true)
+    raw_stores = JSON.parse(response.body, symbolize_names: true)[:stores]
     binding.pry
 
     @stores = raw_stores.map do |raw_store|
