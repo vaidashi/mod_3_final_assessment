3fc1219c3b987968e97f41ff20dd0e056060a216
Ashish Vaid <vaid50@yahoo.com>
2017-11-01 12:09:36 -0600

    modified application controller protect from forgery method in order to get rid of authenticity token error

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,8 +1,32 @@
+#
+# module Api
+#   module V1
+    # class ItemsController < ApplicationController
 
 class Api::V1::ItemsController < ApplicationController
+
 
   def index
     render json: Item.all
   end
 
+  def show
+    render json: Item.find(params[:id])
+  end
+
+  def destroy
+    Item.delete(params[:id])
+  end
+
+  def create
+    render json: Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :image_url)
+  end
 end
+# end
+# end
--- spec/requests/api/vi/item_request_spec.rb
+++ spec/requests/api/vi/item_request_spec.rb
@@ -15,4 +15,42 @@ describe "Items Request API" do
     expect(json.class).to eq(Array)
     expect(json.count).to eq(5)
   end
+
+  it "can get one item" do
+    5.times do |n|
+      Item.create(name: "Item#{n}", description: "description#{n}", image_url: "http://bit.ly/2iSfuZS")
+    end
+
+    id = Item.first.id
+
+    get "/api/v1/items/#{id}.json"
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_success
+  end
+
+  it "can delete an item" do
+    item = Item.create(name: "yoyo", description: "Nice yoyo", image_url: "http://bit.ly/2iSfuZS")
+
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{item.id}"
+
+    expect(response).to be_success
+    expect(Item.count).to eq(0)
+  end
+
+  it "can create a new item" do
+    item_params = {name: "Toy", description: "Popular Christmas Item", image_url: "http://bit.ly/2iSfuZS"}
+
+    post "/api/v1/items", params: {item: item_params}
+    item = Item.last
+
+    assert_response :success
+    expect(response).to be_success
+    expect(item.name).to eq(item_params[:name])
+    expect(item.description).to eq(item_params[:description])
+  end
+
 end
