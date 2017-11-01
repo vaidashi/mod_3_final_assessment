a705003c6957c130cd71cc556c9fb646acd4028c
Ashish Vaid <vaid50@yahoo.com>
2017-11-01 10:35:52 -0600

    feature passing, will look to refactor into poro's , starting with store service

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,11 +5,30 @@ class SearchController < ApplicationController
     response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name,city,distance,phone&pageSize=20&apiKey=#{ENV["BEST_BUY_KEY"]}")
 
     raw_stores = JSON.parse(response.body, symbolize_names: true)[:stores]
-    binding.pry
+    # binding.pry
 
     @stores = raw_stores.map do |raw_store|
       Store.new(raw_store)
     end
+  end
+
+end
+
+
+class Store
+
+  attr_reader :name,
+              :city,
+              :distance,
+              :phone_number,
+              :store_type
+
+  def initialize(attrs)
+    @name = attrs[:name]
+    @city = attrs[:city]
+    @distance = attrs[:distance]
+    @phone_number = attrs[:phone]
+    @store_type = attrs[:store_type]
   end
 
 end
