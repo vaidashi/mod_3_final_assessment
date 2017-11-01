a69169af4eae7bf518812144c07806848be679c0
Ashish Vaid <vaid50@yahoo.com>
2017-11-01 10:08:15 -0600

    adjusted faraday call page size display

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,10 @@
 class SearchController < ApplicationController
 
   def index
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=2&apiKey=#{ENV["BEST_BUY_KEY"]}")
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=20&apiKey=#{ENV["BEST_BUY_KEY"]}")
 
     raw_stores = JSON.parse(response.body, symbolize_names: true)
-    # binding.pry
+    binding.pry
 
     @stores = raw_stores.map do |raw_store|
       Store.new(raw_store)
